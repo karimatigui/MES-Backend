99c1e3566297724070bd5ec14ceb7f66f2c8d782
karim atigui <karim01atigui@gmail.com>
2025-07-10 11:23:07 +0100

    pip freeze > requirements.txt

--- Backend/settings.py
+++ Backend/settings.py
@@ -5,7 +5,8 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
- 
+print("DATABASE_URL =", repr(DATABASE_URL))
+
  
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
