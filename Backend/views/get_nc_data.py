87d6e6eedda77486567d4afaf7d574d78f505d2a
karim atigui <karim01atigui@gmail.com>
2025-07-11 14:32:53 +0100

    database

--- Backend/views/get_nc_data.py
+++ Backend/views/get_nc_data.py
@@ -1,33 +1,18 @@
-# views.py
-from rest_framework.decorators import api_view # type: ignore
-from rest_framework.response import Response # type: ignore
-import requests  # type: ignore
-from Backend.utils.token_manager import get_mingle_token
-from django.http import JsonResponse
-from rest_framework import status
-from Backend.models import IonAPICredentials
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.hashers import make_password
-
-
-
 @api_view(['GET'])
 def get_nc_data(request):
     # Step 1: Get access token
-    
-    # token_response = requests.get('http://localhost:8000/api/get-token/')
     token_response = requests.get('https://mes-backend-1.onrender.com/api/get-token/')
     if token_response.status_code != 200:
         return Response({'error': 'Failed to get token'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     access_token = token_response.json().get('access_token')
 
-          # Step 2: Get company code dynamically
-    company_response = requests.get('http://127.0.0.1:8000/api/get_ionapi_credential/')
-    company_response.raise_for_status()
-    company_code = 5100 
-    # company_response.json().get('company')
+    # Step 2: Get company code dynamically
+    company_response = requests.get('https://mes-backend-1.onrender.com/api/get_ionapi_credential/')
+    if company_response.status_code != 200:
+        return Response({'error': 'Failed to get company'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+    company_code = company_response.json().get('company')
     if not company_code:
         return Response({'error': 'Company code missing in credentials'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
@@ -35,12 +20,11 @@ def get_nc_data(request):
     headers = {
         'Authorization': f'Bearer {access_token}',
         'Accept': 'application/json',
-        'X-Infor-LnCompany': company_code,
+        'X-Infor-LnCompany': 5100,
     }
-    # Step 3: Define base URL
-    url = 'https://mingle-ionapi.eu1.inforcloudsuite.com/LDE4VNS7C63W3JGC_DEM/LN/lnapi/odata/qmapi.ncmNonConformanceReport/NonConformanceReports'
 
-    # Step 4: Recursive fetch loop
+    # Step 4: Fetch from Infor API
+    url = 'https://mingle-ionapi.eu1.inforcloudsuite.com/LDE4VNS7C63W3JGC_DEM/LN/lnapi/odata/qmapi.ncmNonConformanceReport/NonConformanceReports'
     all_results = []
 
     while url:
@@ -50,7 +34,6 @@ def get_nc_data(request):
 
         data = response.json()
         all_results.extend(data.get('value', []))
-        url = data.get('@odata.nextLink')  # Move to next page if exists
+        url = data.get('@odata.nextLink')  # Paginate
 
-    # Step 5: Return combined results
     return Response(all_results, status=status.HTTP_200_OK)
